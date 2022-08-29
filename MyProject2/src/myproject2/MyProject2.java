ee8ac9cecabd26e54575a6fe94dcc23fe4446387
Hasna Nurul Izzati <hasna.nrlzt@student.uns.ac.id>
2022-08-29 22:17:56 +0700

    Latihan 01
    LKPD PBO 01

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author hasna.nrlzt
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("HH    HH  ");
+        System.out.print("  AAAAA  ");
+        System.out.println("  SSSSSS  ");
+        System.out.print("HH    HH  ");
+        System.out.print("AA     AA");
+        System.out.println("  SS      ");
+        System.out.print("HHHHHHHH ");
+        System.out.print(" AA AAAA AA");
+        System.out.println(" SSSSSS");
+        System.out.print("HH    HH ");
+        System.out.print(" AA      AA");
+        System.out.println("     SS");
+        System.out.print("HH    HH ");
+        System.out.print(" AA      AA");
+        System.out.println(" SSSSSS");
+    }
+    
+}
